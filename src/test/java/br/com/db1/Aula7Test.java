a71451f9adc3d9c18b269553e0cb593d3bddae6f
Marcelo <marceloasf1999@gmail.com>
2018-08-17 16:45:46 -0300

    Modificações

--- src/test/java/br/com/db1/Aula7Test.java
+++ src/test/java/br/com/db1/Aula7Test.java
@@ -1,33 +1,5 @@
 package br.com.db1;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import br.com.db1.type.Aula7Brasil;
-
 public class Aula7Test {
-	Aula7Brasil chamaMetodoEnum;
-	
-	
-	@Test
-	public void retornaEstadosDoSulTeste(){
-		//System.out.println(chamaMetodoEnum.retornaEstadosDoNorte());
-		//Assert.assertEquals("Paraná", chamaMetodoEnum.retornaEstadosDoSul().get(0));
-	}
-	@Test
-	public void retornaEstadosDoSudesteTeste(){
-		
-	}
-	@Test
-	public void retornaEstadosDoCentroOesteTeste(){
-		
-	}
-	@Test
-	public void retornaEstadosDoNorteTeste(){
-		
-	}
-	@Test
-	public void retornaEstadosDoNordesteTeste(){
-		
-	}
+
 }
