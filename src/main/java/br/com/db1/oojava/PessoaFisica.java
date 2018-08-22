34e884db83f52b3fdbe96383938e76af5d045ba8
marcelo <marceloasf1999@gmail.com>
2018-08-22 17:37:49 -0300

    Duas adições ao cod, que são pessoa juridica e fisica

---
+++ src/main/java/br/com/db1/oojava/PessoaFisica.java
@@ -0,0 +1,16 @@
+package br.com.db1.oojava;
+
+public class PessoaFisica extends Pessoa{
+	private String cpf;
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	//FUM validador CPF
+	
+}
---
+++ src/main/java/br/com/db1/oojava/PessoaJuridica.java
@@ -0,0 +1,16 @@
+package br.com.db1.oojava;
+
+public class PessoaJuridica extends Pessoa{
+	private String cnpj;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	//FUM validador CNPJ
+	
+}
